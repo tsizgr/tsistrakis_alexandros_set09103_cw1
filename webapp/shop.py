c31756da879f406c0aa511b4a5f57648aea19fa3
Alexandros Tsistrakis <atsistrakis@gmail.com>
2018-10-12 22:36:51 +0100

    Projectttt2

--- webapp/shop.py
+++ webapp/shop.py
@@ -1,9 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, #request
 app = Flask (__name__)
 
 @app.route ('/')
 def home ():
-	return render_template ('template.html')
+		return render_template ('template.html')
+		
+@app.route ('/photos')
+def photos ():
+	start = '<img src="'
+	url = url_for('static', filename='bigsales.jpg')
+	end ='">'
+	return start+url+end, 200
+	
+	
+# @app.route('/account/', methods=['POST','GET'])
+#def account():
+#	if request.method == 'POST':
+#		print request.form
+#		name = request.form['name']
+#		return "Hello %s" % name
+#	else:
+#		page ='''
+#		<html > <body >
+#		<form action ="" method =" post " name ="form">
+#		<label for =" name " > Name : </ label >
+#		<input type =" text " name =" name " id ="name"/>
+#		<input type =" submit " name =" submit " id =" submit"/>
+#		</form>
+#		</body><html>'''
+#
+#	return page
+
 	
 @app . route ('/ force404 ')
 def force404 ():
